653ebc840933747a4457df7d1dfde613196eb5dc
s <s.looi2@outlook.com>
2023-11-30 18:37:23 +1300

    refactor code

--- src/server/route.ts
+++ src/server/route.ts
@@ -1,42 +1,83 @@
 import express from "express";
 import {Request, Response} from "express";
+import { z } from "zod";
 
 const r_api = express.Router()
 
 
 const c_estimateCarValue = (req:Request,res:Response)=>{
-	res.send("hi")
-	// try{
-	// 	// Validate unsafe input
-	// 	const validatedQuery = v_estimateCarValue.parse(req.query)
+	try{
+		// Validate unsafe input
+		const validatedQuery = v_estimateCarValue.parse(req.query)
+		console.log(validatedQuery)
+		console.log(validatedQuery)
+		console.log(validatedQuery)
+		console.log(validatedQuery)
+		
+		const carValue = s_estimateCarValue(validatedQuery)
+		res.json({car_value:carValue})
+	}catch (err){
+		console.log(err)
+		res.json({error:"there was an error"})
+	}
+}
+const v_estimateCarValue = z.object({
+	model: z.string().min(1,"String length must be > 0"),
 
-	// 	// 
-	// 	const carValue = s_estimateCarValue(validatedQuery)
-	// 	res.json({car_value:carValue})
-	// }catch{
-	// 	res.json({error:"there was an error"})
-	// }
+	year: z.union([
+		z.string()
+		.refine((stringNumber)=>{
+			const numberOrNan = Number(stringNumber);
+			return !isNaN(numberOrNan)
+		},{message:"String was not able to be parsed into a number!"})
+		.transform((stringNumber) => {
+			return Number(stringNumber)
+		}),
+		z.number()
+	])
+	.refine((number)=>{
+		return number>=0
+	},{message:"Year must be a positive number"}),
+});
+
+type t_estimateCarValue = z.infer<typeof v_estimateCarValue>
+
+const s_estimateCarValue = (estimateCarValueInput:t_estimateCarValue):number=>{
+	const {model,year}=v_estimateCarValue.parse(estimateCarValueInput)
+
+	let carValue:number = 0
+
+	// Calculate car value from alphabet position
+	for(let i=0;i<model.length;i++){
+		carValue += s_getAlphabetPosition(model[i]) * 100
+	}
+
+	// Add year price
+	carValue += year
+
+	// Return
+	return carValue
 }
 
-// const estimateCarValue = (estimateCarValueInput:t_estimateCarValue):number=>{
-// 	const {model,year}=v_estimateCarValue.parse(estimateCarValueInput)
 
-// 	let carValue:number = 0
+const s_getAlphabetPosition = (letter:string)=>{
+	// Expects a string with length 1 representing a single alphabet
+	// Returns the position of alphabet (eg: a=>1,A=>1,Z=>26)
 
-// 	// Calculate car value from alphabet position
-// 	for(let i=0;i<model.length;i++){
-// 		carValue += s_getAlphabetPosition(model[i]) * 100
-// 	}
+	// Check
+	if (letter.length > 1) throw new Error("ERROR_ARGUMENT_LENGTH_NOT_ONE")
 
-// 	// Add year price
-// 	carValue += year
+	// Determine position of alphabet
+	const charCode:number = letter.toLowerCase().charCodeAt(0)
+	if (charCode >= 97 && charCode <= 97+25){
+		return charCode-96
+	}
 
-// 	// Return
-// 	return carValue
-// }
-
+	// Return
+	return 0
+}
 
 // Routes -> Controllers
 r_api.get("/estimate-car-value",c_estimateCarValue)
 
-export default r_api
+export{ r_api}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,20 +1,12 @@
 import express from "express"
 import path from "path"
-// import {fileURLToPath} from 'url';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = require('path').dirname(require.main.filename);
-
+import {r_api} from "./route.js"
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
 app.use(express.static(path.join(process.cwd(),"dist")))
 
-app.get("/a",(req,res)=>{
-	res.send("<h1>a route</h1>")
-})
-app.get("/a2",(req,res)=>{
-	res.send("<h1>a2 routes345</h1>")
-})
+app.use(r_api)
 
 app.listen(PORT,()=>console.log("LISTENING ON PORT"+PORT))
